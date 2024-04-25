9d7d63cdf6920edaf25c52ae7e6763e6fb0ba97b
Vlad Chernukha <forreadtr@gmail.com>
2024-04-25 22:13:28 +0300

    Create struct Employee

---
+++ HomeTask1/Employee.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct Employee
+{
+	char surname[100] = "";
+	float frequency = 0;
+	int BirthYear = 0;
+	char position[100] = "";
+	float salary = 0;
+	char education[100] = "";
+
+};
