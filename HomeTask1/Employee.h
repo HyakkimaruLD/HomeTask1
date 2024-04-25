3d12c55dd0df167c41ba91a53071bbcecd2f0e89
Vlad Chernukha <forreadtr@gmail.com>
2024-04-25 22:20:18 +0300

    Create massiv Employee

--- HomeTask1/Employee.h
+++ HomeTask1/Employee.h
@@ -6,10 +6,18 @@ using namespace std;
 struct Employee
 {
 	char surname[100] = "";
-	float frequency = 0;
 	int BirthYear = 0;
 	char position[100] = "";
 	float salary = 0;
 	char education[100] = "";
 
+
+	void showInfo()
+	{
+		cout << "Surname: " << surname << endl;
+		cout << "Birth year: " << BirthYear << endl;
+		cout << "Position: " << position << endl;
+		cout << "Salary: " << salary << "UAH" << endl;
+		cout << "Education: " << education << endl;
+	}
 };
--- HomeTask1/HomeTask1.cpp
+++ HomeTask1/HomeTask1.cpp
@@ -6,6 +6,15 @@
 using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int N = 5;
+
+    Employee* arr = new Employee[N]
+    {
+        { "Vasilich", 1876, "CEO", 150'000, "Best" },
+        { "Annavna", 1990, "CTO", 120'000, "TechSavvy" },
+        { "Hiroshivna", 1985, "CFO", 130'000, "FinanceWhiz" },
+        { "Sophievna", 1988, "COO", 125'000, "OperationsGuru" },
+        { "Nikolaivna", 1993, "CMO", 110'000, "MarketingMaestro" }
+    };
 }
 
